381949d1b6ca25fdcf004d7b6ddc204fac56b4c9
Otavio <suizld@pm.me>
2024-12-03 16:53:14 -0300

    ported configTemplate file to TypeScript
    Ported config template object to TypeScript and added its interface type (structure)

--- app/configTemplate.ts
+++ app/configTemplate.ts
@@ -7,16 +7,28 @@ FCapture
 
 */
 
+// config object interface
+export interface IConfigObjectStructure {
+  videoMode: string;
+  imageRenderingPriority: "auto" | "crisp-edges" | "pixelated" | "optimizeQuality" | "optimizeSpeed" | "smooth";
+  imageBrightness: number;
+  imageContrast: number;
+  imageSaturation: number;
+  debugOverlay: boolean;
+  audioMode: string;
+  surroundAudio: boolean;
+  bassBoost: boolean;
+}
+
 // default config object template
-export let configObjectTemplate = {
+export const configObjectTemplate: IConfigObjectStructure = {
   videoMode: "",
   imageRenderingPriority: "auto",
-  imageBrightness: 100, // percentage (ranges from 0% to 200%)
-  imageContrast: 100, // percentage (ranges from 0% to 200%)
-  imageSaturation: 100, // percentage (ranges from 100% to 200%)
+  imageBrightness: 100, // percentage (ranges from 0 to 200)
+  imageContrast: 100, // percentage (ranges from 0 to 200)
+  imageSaturation: 100, // percentage (ranges from 0 to 200)
   debugOverlay: false,
-  //
   audioMode: "",
   surroundAudio: false,
-  bassBoost: false
+  bassBoost: false,
 };
